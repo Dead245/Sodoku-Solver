4bd7d9aba962a7d556cd1bdf29f2e52714af4a5c
Dead245 <116427900+Dead245@users.noreply.github.com>
2023-09-08 03:30:29 -0500

    removed unneeded function in Validator

--- SodokuSolver/Validator.cs
+++ SodokuSolver/Validator.cs
@@ -36,51 +36,7 @@ namespace SodokuSolver
                 columnSet.Clear();
                 boxSet.Clear();
             }
-
-            
-
-
             return true;
-        }
-
-        //Assuming row/column starts as a range of 0-8
-        private int findSubGrid(int column, int row) {
-            //Gives what % of 9 is each int
-            column = column / 9;
-            row = row / 9;
-
-            //Checks where the % lies and sets it to that range of 1/2/3
-            switch (column) {
-                case int n when (n < 0.33):
-                    column = 1;
-                    break;
-
-                case int n when (n >= 0.33 && n < 0.66):
-                    column = 2;
-                    break;
-
-                case int n when (n < 1):
-                    column = 3;
-                    break;
-            }
-
-            switch (row)
-            {
-                case int n when (n < 0.33):
-                    row = 1;
-                    break;
-
-                case int n when (n >= 0.33 && n < 0.66):
-                    row = 2;
-                    break;
-
-                case int n when (n < 1):
-                    row = 3;
-                    break;
-            }
-
-            return ((row - 1) * 3) + column;
-        }
-    
+        }    
     }
 }
