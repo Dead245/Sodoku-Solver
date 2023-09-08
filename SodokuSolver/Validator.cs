e0a794975cf21f8ce8f5d5184322788e11c5e1de
Dead245 <116427900+Dead245@users.noreply.github.com>
2023-09-08 03:29:15 -0500

    Rewrote Validator into 1 double for loop
    By using the char[,] board

--- SodokuSolver/Validator.cs
+++ SodokuSolver/Validator.cs
@@ -13,24 +13,33 @@ namespace SodokuSolver
         public bool validateNewBoard(char[,] board) {
 
 
-            //Check
-            var set = new HashSet<char>();
+            //Check Rows & Columns & Subgrids for Duplicate Numbers
+            var rowSet = new HashSet<char>();
+            var columnSet = new HashSet<char>();
+            var boxSet = new HashSet<char>();
             for (int y = 0; y < 9; y++)
             {
                 for (int x = 0; x < 9; x++)
-                {
-                   
+                {       //Row Check
+                    if (!rowSet.Add(board[x,y]) && board[x,y] != ' ') {
+                        return false;
+                    }   //Column Check
+                    if (!columnSet.Add(board[y,x]) && board[y,x] != ' ') {
+                        return false;
+                    }   //Subgrid Check
+                    if (!boxSet.Add(board[(int)(3 * Math.Floor(y / 3.0) + Math.Floor(x / 3.0)), ((y * 3) % 9) + (x % 3)]) && 
+                        board[(int)(3 * Math.Floor(y / 3.0) + Math.Floor(x / 3.0)), ((y * 3) % 9) + (x % 3)] != ' ') {
+                        return false;
+                    }
                 }
+                rowSet.Clear();
+                columnSet.Clear();
+                boxSet.Clear();
             }
 
             
 
 
-            return true;
-        }
-
-        private bool checkSubGrid(int subGridNum) {
-            double gridPoint;
             return true;
         }
 
